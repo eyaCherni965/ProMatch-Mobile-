928c0bbe4fa46f9e40254f65b4187a08bd27a5dc
rbhunjun22 <jyotsna-lakshmi.bhunjun.1@ens.etsmtl.ca>
2025-03-27 14:23:58 -0400

    build gradle ++

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,10 +41,15 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
 
-    implementation("com.microsoft.sqlserver:mssql-jdbc:9.4.0.jre8") 
+    implementation("com.microsoft.sqlserver:mssql-jdbc:9.4.0.jre8")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+
 }
 
